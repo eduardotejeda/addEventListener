f3857330641e3f9f69e4620b2a3c0d71f17ec457
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2022-11-20 06:12:48 -0400

    Add multiple events lisener using js

--- app.js
+++ app.js
@@ -19,3 +19,20 @@ btn.addEventListener('mouseout', function() {
 window.addEventListener('scroll', function() {
 	console.log('STOP SCROLLING!!');
 });
+
+
+const btn2 = document.querySelector('#new')
+
+btn2.addEventListener('mouseover', function() {
+	console.log('Mouse over me');
+	const height = Math.floor(Math.random() * window.innerHeight);
+	const width = Math.floor(Math.random() * window.innerWidth);
+	btn2.style.left = `${width}px`;
+	btn2.style.top = `${height}px`;
+
+});
+
+btn2.addEventListener('click', function() {
+	btn2.innerText = 'You got me!';
+	document.body.style.backgroundColor = 'green';
+})
